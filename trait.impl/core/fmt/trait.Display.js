328e11616eacb8b94dcc4d6f68a93ee885c6e4ea
Dickhat <Dickhat@users.noreply.github.com>
2025-11-15 13:20:19 +0000

    deploy: 147270e18245dee543d8a711957d081d3f876782

--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["CyberSecurity",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"CyberSecurity/gui/cryptography/enum.KuznechickModes.html\" title=\"enum CyberSecurity::gui::cryptography::KuznechickModes\">KuznechickModes</a>"]]]]);
+    var implementors = Object.fromEntries([["CyberSecurity",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"CyberSecurity/gui/cryptography/enum.KuznechickModes.html\" title=\"enum CyberSecurity::gui::cryptography::KuznechickModes\">KuznechickModes</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
